fb0c0b05f711409a8f27bcc0453081afb06ce6fd
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2021-03-29 09:27:04 -0400

    main router

--- login-frontend/src/App.js
+++ login-frontend/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MainRouter from "./MainRouter";
+import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
+import "react-toastify/dist/ReactToastify.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    let getJwtToken = localStorage.getItem("jwtToken");
+    if (getJwtToken) {
+      const currentTime = Date.now() / 1000;
+
+      let decodedJwtToken = jwtDecode(getJwtToken);
+
+      if (decodedJwtToken.exp < currentTime) {
+        this.handleUserLogout();
+      } else {
+        this.handleUserLogin(decodedJwtToken);
+      }
+    }
+  }
+
+  handleUserLogin = (user) => {
+    this.setState({
+      user: {
+        email: user.email,
+      },
+    });
+  };
+
+  handleUserLogout = () => {
+    localStorage.removeItem("jwtToken");
+    this.setState({
+      user: null,
+    });
+  };
+
+
+  render() {
+    return (
+      <>
+      <ToastContainer />
+      <MainRouter
+        user={this.state.user}
+        handleUserLogin={this.handleUserLogin}
+        handleUserLogout={this.handleUserLogout}
+      />
+    </>
+    )
+  }
 }
 
-export default App;
+export default App
